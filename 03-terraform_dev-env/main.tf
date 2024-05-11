bf5ee74dcfd9efa5f16f98f2d55d2009e669a53f
uzumakiruto017 <quannar178@gmail.com>
2024-05-11 17:42:20 +0700

    associate route table vs subnet, security group

--- 03-terraform_dev-env/main.tf
+++ 03-terraform_dev-env/main.tf
@@ -37,7 +37,32 @@ resource "aws_route_table" "dev_public_rt" {
 }
 
 resource "aws_route" "defult_route" {
-  route_table_id = aws_route_table.dev_public_rt.id
+  route_table_id         = aws_route_table.dev_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.dev_internet_gateway.id
+  gateway_id             = aws_internet_gateway.dev_internet_gateway.id
+}
+
+resource "aws_route_table_association" "dev_public_assoc" {
+  subnet_id      = aws_subnet.dev_public_subnet.id
+  route_table_id = aws_route_table.dev_public_rt.id
+}
+
+resource "aws_security_group" "dev_sg_allow_tls" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
