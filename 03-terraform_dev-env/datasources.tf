6b1b94dab44d55a083c068cef97f4bed35edb265
uzumakiruto017 <quannar178@gmail.com>
2024-05-11 17:48:43 +0700

    add data souce ami

---
+++ 03-terraform_dev-env/datasources.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "server_ami" {
+  most_recent = true
+  owners      = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.4.20240429.0-kernel-6.1-x86_64"]
+  }
+}
